d1da29a0252ac7d82953cc1f6ba146bff0a4e6b5
denpro <denpro87@gmail.com>
2021-01-31 19:47:13 +0200

    second commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -50,7 +50,7 @@ router.post('/users', async (req, res) => {
         sendWelcomeEmail(user.email, user.name)
         res.status(201).send({user, token})
     } catch (err){
-        res.status(400).send(err)
+        res.status(400).send(err + "something went wrong")
     }
 })
 
